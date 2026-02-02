a78aa970587a8eed7bc13b36827a5e8d82d4e4d7
JesusT491 <jtorres217@cps.edu>
2026-02-02 14:53:13 -0600

    Add files via upload

---
+++ jokes/PerformanceTask-Test---Jokes2025/test.py
@@ -0,0 +1,54 @@
+#test
+
+joke = input("Do you want to hear a joke (Yes or No)?: ")
+
+# Defining our Functions
+def rate_game():
+    rate = int(input("Please rate our game 1-10! "))
+    final_score = int(rate * 10)
+    print(str(final_score) + " percent satisfaction rate")
+    friend = input("Would you recommend this game to a friend? ")
+
+    if friend == "yes" or friend == "maybe":
+        print("Thanks, we appreciate it. ")
+    else:
+        print("Sorry you did not enjoy it. ")
+
+def robber_joke(choice1):
+    input("Knock Knock ")
+    input("Calder")
+    print("Calder police - I've been robbed!")
+    joke = input("Do you want to hear another joke or are you finished? ")
+    return 1
+
+def tanks_joke(choice2):
+    input("Knock Knock ")
+    input("Tank ")       
+    input("You are welcome! ")
+    joke = input("Do you want to hear another joke or are you finished? ")
+    return 2
+
+def pencils_joke(choice3):
+    input("Knock Knock ")
+    input("Broken pencil ")        
+    input("Nevermind, it's pointless! ")
+    joke = input("Do you want to hear another joke or are you finished? ")
+    return 3
+
+#main loop
+
+joke_numeral = [1,2,3]
+
+while joke == "no":
+    
+    question = input("Do you want to hear a joke about robbers, tanks, or pencils? ")
+    for choice in joke_numeral:
+        if choice == 1:
+            robber_joke(1)
+        elif choice == 2:
+            tanks_joke(2)
+        elif choice == 3:
+            pencils_joke(3)
+
+#when you leave the while loop, this function runs
+rate_game()
