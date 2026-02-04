20b73fb45b38f4e916c45325e4c27f8285c53aae
JesusT491 <jtorres217@cps.edu>
2026-02-04 20:21:56 +0000

    final2

--- jokes/PerformanceTask-Test---Jokes2025/Main Version.py
+++ jokes/PerformanceTask-Test---Jokes2025/Main Version.py
@@ -1,6 +1,9 @@
-#test
+#Main Version
 
 joke = input("Do you want to hear a joke (Yes or No)?: ").lower()
+
+#this list 
+jokes_told = []
 
 # Defining our Functions
 
@@ -24,22 +27,21 @@ def pencils_joke():
     input("Nevermind, it's pointless! ")
     return  input("Do you want to hear another joke or are you finished? ").lower()
 
-def rate_game(name):
+def rate_game(name,jokes_told):
+    total = len(jokes_told) 
     rate = int(input("Please rate our game 1-10! "))
     final_score = int(rate * 10)
     print(str(final_score) + " percent satisfaction rate")
+    print(f'you were told {total} jokes')
     friend = input("Would you recommend this game to a friend? ").lower()
 
     if friend == "yes" or friend == "maybe":
-        print(f"Thanks, we appreciate it {name}. ")
+        print("Thanks, we appreciate it. ")
     else:
-        print(f"Sorry you did not enjoy it {name}. ")
+        print("Sorry you did not enjoy it. ")
 
 
 #main loop
-
-#this list tracks all of our functions so that they can be ran later on
-joke_list = [robber_joke, tanks_joke, pencils_joke]
 
 while joke == "yes":
     
@@ -48,12 +50,16 @@ while joke == "yes":
 
     #each joke will play depending on the number that was picked. As well, the joke variable is used later for
     #when the user no longer wants to hear the joke, it will get them out of the loop then plays our rate game function
+    
     if choices == '1':
-        joke = robber_joke()        
+        joke = robber_joke()
+        jokes_told.append(1)
     elif choices == '2':
         joke = tanks_joke()
+        jokes_told.append(1)
     elif choices == '3':
         joke = pencils_joke()
+        jokes_told.append(1)
 
 #when you leave the while loop, this function runs
-rate_game('jimmy')
+rate_game(jokes_told)
